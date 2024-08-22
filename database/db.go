0b00a0d5c16f28002452fb258da4171ae07c55a6
Tales Palma <tales.palma_2022@outlook.com>
2024-08-22 18:45:22 -0300

    Add .env

--- database/db.go
+++ database/db.go
@@ -1,7 +1,12 @@
 package database
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/TalesPalma/gin-golang-rest/models"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -12,10 +17,20 @@ var (
 )
 
 func ConnectionDatabase() {
-	dsn := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DB")
+
+	dsn := fmt.Sprintf("host=localhost user=%s password=%s dbname=%s port=5432 sslmode=disable", user, password, dbName)
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
+		fmt.Println("Teste", user, password, dbName)
 		panic("failed to connect database")
 	}
 
